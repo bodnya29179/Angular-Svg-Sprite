8ab3f31655fede97b70d07020f02559de015d55c
Yevhen Bodnia <bodnya29179@gmail.com>
2023-04-16 14:54:52 +0300

    Add icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <svg-icon [src]="'angular'"></svg-icon>
+    <svg-icon class="icon angular-icon" [src]="'angular'"></svg-icon>
+
+    <svg-icon class="icon nodejs-icon" [src]="'nodejs'"></svg-icon>
+
+    <svg-icon class="icon power-icon" [src]="'power'"></svg-icon>
+
+    <svg-icon class="icon youtube-icon" [src]="'youtube'"></svg-icon>
+
+    <svg-icon class="icon apple-icon" [src]="'apple'"></svg-icon>
+
+    <svg-icon class="icon heart-icon" [src]="'heart'"></svg-icon>
   `,
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {}
