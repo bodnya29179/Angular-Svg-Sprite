169529922830ff6f47f755d43db0374d942af8f5
Yevhen Bodnia <bodnya29179@gmail.com>
2024-08-02 14:52:18 +0300

    Handle gradient ids in svg icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
+    <!-- Icons for changing colors by using ::ng-deep -->
     <svg-icon class="icon angular-icon" [src]="'angular'"></svg-icon>
 
     <svg-icon class="icon nodejs-icon" [src]="'nodejs'"></svg-icon>
@@ -14,6 +15,16 @@ import { Component } from '@angular/core';
     <svg-icon class="icon apple-icon" [src]="'apple'"></svg-icon>
 
     <svg-icon class="icon heart-icon" [src]="'heart'"></svg-icon>
+
+    <!-- Linear gradient icons with unique gradient IDs -->
+    <svg-icon class="icon" [src]="'linear-gradient'"></svg-icon>
+
+    <svg-icon class="icon" [src]="'linear-gradient'"></svg-icon>
+
+    <!-- Radial gradient icons with unique gradient IDs -->
+    <svg-icon class="icon" [src]="'radial-gradient'"></svg-icon>
+
+    <svg-icon class="icon" [src]="'radial-gradient'"></svg-icon>
   `,
   styleUrls: ['./app.component.scss'],
 })
